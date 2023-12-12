a0565d7d0a976af1738d430d870d6eb7509afcc0
chenwj <853064501@qq.com>
2023-12-12 09:46:47 +0800

    fix: 去除无用代码

--- src/views/chatComponents/voiceChat.vue
+++ src/views/chatComponents/voiceChat.vue
@@ -9,17 +9,13 @@
 
     <div class="chatWrapper">
       <div class="instruct" >您的指令是： <span >{{textData}}</span>
-<!--        <v-md-editor v-model="textRes" height="400px" />-->
         <v-md-preview :text="textRes" />
       </div>
-      <canvas ref="canvas" class="canvas"></canvas>
 
       <div class="btnOut" @click="handleChat"  >
-
         <img src="../../assets/huatong.png"
              width="68">
         <p class="btnText">{{start?'开始':'结束'}}</p>
-
       </div>
 
       <div  class="musical-scale">
@@ -28,14 +24,6 @@
         </div>
       </div>
 
-<!--      <p ref="status"></p>-->
-<!--      <p ref="output"></p>-->
-
-      <audio ref="audio" style="display: none" :src="mp3" @play="play" @pause="audioEnded" @ended="audioEnded"
-             controls></audio>
-
-
-
     </div>
 
 
@@ -43,7 +31,6 @@
 </template>
 
 <script setup >
-import mp3 from './1.mp3'
 import IatRecorder from '@/utils/IatRecorder.js'
 import axios from "axios";
 import XunfeiReader from '@/utils/js/index'
@@ -53,11 +40,8 @@ defineOptions({
   name: 'VoiceChat',
 })
 
-const audio = ref()
-const canvas = ref()
 const dialogVisible = ref(false)
-const audCon = ref()
-const source = ref()
+
 
 const APPID = 'fe55ce9c'
 const API_SECRET = 'NDQ0YWU5OTIwNTU0YTQwYTc5ZDY3YjM5'
@@ -72,9 +56,9 @@ const textRes = ref('')
 // 录音开始
 const startListen = ()=>{
   textData.value = ''
-  // Observe.observe('event',log)
   iatRecorder.start()
   let seconds = 0
+
   const count = setInterval(() => {
     seconds++
     if (seconds >= 60) {
@@ -83,34 +67,21 @@ const startListen = ()=>{
       clearInterval(count)
     }
   }, 1000)
+
   iatRecorder.onWillStatusChange = function(oldStatus, newStatus){
     if(newStatus==='end') {
       console.log('结束听写', this.text)
-      askWenxin(this.text)
+      setTimeout(() => {
+        askWenxin(this.text)
+      }, 1000)
+
     }
   }
-  // iatRecorder.onTextChange = function (text) {
-  //   console.log('text-----------', text)
-  //   let inputText = text
-  //   textData.value = inputText.substring(0, inputText.length - 1) //文字处理，因为不知道为什么识别输出的后面都带‘。’，这个方法是去除字符串最后一位
-  //   iatRecorder.stop()
-  //   askWenxin(text)
-  // }
+
 }
 // 录音结束
 const startListenT = () => {
-
-  setTimeout(() => {
-    iatRecorder.stop()
-  }, 1000)
-
-  // iatRecorder.onTextChange = function (text) {
-  //   console.log('text', text)
-  //   let inputText = text
-  //   textData.value = inputText.substring(0, inputText.length - 1) //文字处理，因为不知道为什么识别输出的后面都带‘。’，这个方法是去除字符串最后一位
-  //   iatRecorder.stop()
-  //   askWenxin(text)
-  // }
+  iatRecorder.stop()
 }
 
 const askWenxin = async (mes)=>{
@@ -121,8 +92,8 @@ const askWenxin = async (mes)=>{
     conv_uid: '49cff5ea-97f2-11ee-a134-5405dbf57f3c',
     user_input: mes,
     model_name: 'wenxin_proxyllm',
-    chat_mode: 'chat_normal',
-    select_param: ''
+    chat_mode: 'chat_knowledge',
+    select_param: 'MES'
   };
   // console.log('process.env.VITE_APP_API_BASE_URL',process.env.VITE_APP_API_BASE_URL)
   try {
@@ -148,13 +119,13 @@ const askWenxin = async (mes)=>{
       onmessage: (event) => {
         const message = event.data?.replaceAll('\\n', '\n');
         if (message === '[DONE]') {
-          // historyList.value.push('')
         } else if (message?.startsWith('[ERROR]')) {
           // textRes.value = message?.replace('[ERROR]', '')
           // XunfeiReader(textRes.value)
         } else {
-          console.log('message', message)
-          textRes.value = message
+          const endIndex = message.indexOf('<references>')
+          console.log(endIndex)
+          textRes.value = endIndex !== -1 ? message.slice(0, endIndex) : message
         }
       },
     });
@@ -166,10 +137,7 @@ const askWenxin = async (mes)=>{
 
 }
 
-const speak = (text) => {
-  const msg = new SpeechSynthesisUtterance(text);
-  window.speechSynthesis.speak(msg);
-}
+
 
 const handleChat = () => {
   if (start.value) {
@@ -178,108 +146,19 @@ const handleChat = () => {
   } else {
     startListenT()
     start.value = true
-
   }
 }
 
 
 const handleShow = () => {
   dialogVisible.value = true
-   nextTick(()=>{
-
-    // init()
-    // audio.value.play()
-    // isStart.value = true
-  })
-
 }
+
 const handleClose = () => {
   dialogVisible.value = false
 }
 
-// 定义数组和音频分析器
-let dataArray = ref(), analyser = ref()
 
-
-const init = () => {
-  // 初始化
-
-  // 初始化canvas的尺寸
-  canvas.value.width = window.innerWidth // 窗口的内部宽度
-  canvas.value.height = window.innerHeight / 2 //窗口的内部高度
-  audCon.value = new AudioContext() // 创建音频上下文
-  source.value = audCon.value.createMediaElementSource(audio.value) // 创建音频源节点
-}
-
-
-
-const openAudio = () => {
-  if (isStart.value) {
-    audio.value.pause()
-    isStart.value = false
-  } else {
-    audio.value.play()
-    isStart.value = true
-  }
-}
-const play = () => {
-  // 创建音频分析器
-  analyser.value = audCon.value.createAnalyser()
-  // 快速傅里叶变换的长度必须为2的整数幂
-  analyser.value.fftSize = 512 // 这里把快速傅里叶变换的长度设置为512
-  // 连接音频源节点到音频分析器上
-  source.value.connect(analyser.value)
-  // 连接音频分析器到音频终端上
-  analyser.value.connect(audCon.value.destination)
-  // 创建数组，用于接收分析器节点的分析数据
-  // Uint8Array 数组类型表示一个 8 位无符号整型数组
-  // frequencyBinCount是fftSize值的一半作为可视化音频的数组长度
-  dataArray.value = new Uint8Array(analyser.value.frequencyBinCount)
-  draw()
-
-}
-
-const draw = () => {
-  const con = canvas.value.getContext('2d')
-// 把分析出的波形绘制到canvas上
-  // 动画帧，按帧绘制canvas
-  requestAnimationFrame(draw)
-  // 清空画布
-  const {width, height} = canvas.value
-  // if(!width) return
-  con.clearRect(0, 0, width, height)
-  // 让分析器节点分析出数据到数组中
-  analyser.value.getByteFrequencyData(dataArray.value)
-  // 设置canvas上下文绘制的颜色
-  con.fillStyle = 'skyblue'
-  // len表示获取分析到的音频数据数组长度的
-  // 这里除以2.5是剔除不经常出现的高频的部分
-  const len = dataArray.value.length / 2.5
-  // barWidth表示每个波形矩形的宽度
-  // 这里除以2是为了绘制对称的波形图
-  const barWidth = width / len / 2
-  // const barWidth = 20
-  for (let i = 0; i < len; i++) {
-    // data是8位数组的每个数据，因为是一个字节，即data的值都是 <= 255
-    const data = dataArray.value[i]
-    // barHeight表示每个波形矩形的高度，值为单位长度乘canvas容器的高
-    const barHeight = (data / 255) * height
-    // 绘制点y
-    const y = height - barHeight
-    // 绘制点x1
-    const x1 = i * barWidth + width / 2
-    // 绘制点x2
-    const x2 = width / 2 - (i + 1) * barWidth
-    // 绘制右半部分波形图
-    con.fillRect(x1, y, barWidth - 2, barHeight)
-    // 绘制左半部分波形图
-    con.fillRect(x2, y, barWidth - 2, barHeight)
-  }
-}
-
-const audioEnded = () => {
-
-}
 
 defineExpose({
   handleShow
--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,3 +1,5 @@
 declare module '@kangc/v-md-editor/lib/preview'
 
 declare module '@kangc/v-md-editor/lib/theme/vuepress.js'
+
+declare module '@kangc/v-md-editor/lib/theme/github.js'
